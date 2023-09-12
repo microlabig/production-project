bb623b03457772840d0f314d4258853109785a1e
Igor.bezmestin <igor-rock@list.ru>
2023-09-12 21:23:13 +0300

    fix: remove Counter component from Main page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,15 +1,9 @@
 import { useTranslation } from 'react-i18next';
-import { Counter } from 'entities/Counter';
 
 function MainPage() {
     const { t } = useTranslation('main');
 
-    return (
-        <div className="wrapper">
-            {t('Главная страница')}
-            <Counter />
-        </div>
-    );
+    return <div className="wrapper">{t('Главная страница')}</div>;
 }
 
 export default MainPage;
