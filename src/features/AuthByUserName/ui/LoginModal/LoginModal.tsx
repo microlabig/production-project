601935e7fa231c64c365434b9563a8e74c068670
Igor.bezmestin <igor-rock@list.ru>
2023-09-14 21:48:53 +0300

    fix: added husky

--- src/features/AuthByUserName/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUserName/ui/LoginModal/LoginModal.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 
 import { Modal } from 'shared/ui/Modal/Modal';
-import { classNames } from 'shared/lib/classNames/classNames';
 import { LoginForm } from '../LoginForm/LoginForm';
-
-import cls from './LoginModal.module.scss';
 
 type TLoginModalProps = {
     isOpen: boolean;
@@ -15,12 +12,7 @@ type TLoginModalProps = {
 
 export function LoginModal(props: TLoginModalProps) {
     return (
-        <Modal
-            isLazy
-            isOpen={props.isOpen}
-            onClose={props.onClose}
-            className={classNames(cls.LoginModal, {}, [props.className])}
-        >
+        <Modal isLazy isOpen={props.isOpen} onClose={props.onClose} className={props.className}>
             <LoginForm />
         </Modal>
     );
