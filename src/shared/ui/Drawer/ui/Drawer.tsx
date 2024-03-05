cabd528a038d2da734985c7f6c918070f4adbbe2
Igor <igor-rock@list.ru>
2024-03-05 19:20:31 +0300

    fix: added react spring, added dynamic lib loading with AnimationProvider

--- src/shared/ui/Drawer/ui/Drawer.tsx
+++ src/shared/ui/Drawer/ui/Drawer.tsx
@@ -1,7 +1,6 @@
-import { ReactNode, memo } from 'react';
-import { Mods, classNames } from 'shared/lib/classNames/classNames';
-
-import { useModal } from 'shared/lib/hooks/useModal/useModal';
+import { ReactNode, memo, useCallback, useEffect } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AnimationProvider, useAnimationLibs } from 'shared/lib/components/AnimationProvider';
 import { useTheme } from 'shared/providers/theme-provider';
 import { Overlay } from '../../Overlay';
 import { Portal } from '../../Portal/Portal';
@@ -16,33 +15,93 @@ type TDrawerProps = {
     className?: string;
 };
 
-const ANIMATION_DELAY = 300;
+const height = window.innerHeight - 100;
 
-export const Drawer = memo((props: TDrawerProps) => {
-    const { className, children, isOpen, onClose, isLazy } = props;
+export const DrawerContent = memo((props: TDrawerProps) => {
+    const { className, children, isOpen, onClose } = props;
     const { theme } = useTheme();
+    const { Spring, Gesture } = useAnimationLibs();
 
-    const { close, isClosing, isMounted } = useModal({
-        animationDelay: ANIMATION_DELAY,
-        onClose,
-        isOpen,
-    });
+    const [{ y }, api] = Spring.useSpring(() => ({ y: height }));
 
-    const mods: Mods = {
-        [cls.opened]: isOpen,
-        [cls.isClosing]: isClosing,
+    const openDrawer = useCallback(() => {
+        api.start({ y: 0, immediate: false });
+    }, [api]);
+    const closeDrawer = (velocity = 0) => {
+        api.start({
+            y: height,
+            immediate: false,
+            config: { ...Spring.config.stiff, velocity },
+            onResolve: onClose,
+        });
     };
 
-    if (isLazy && !isMounted) {
+    const bind = Gesture.useDrag(
+        ({ last, velocity: [, vy], direction: [, dy], offset: [, oy], cancel }) => {
+            // if the user drags up passed a threshold, then we cancel
+            // the drag so that the sheet resets to its open position
+            if (oy < -70) {
+                cancel();
+            }
+
+            // when the user releases the sheet, we check whether it passed
+            // the threshold for it to close, or if we reset it to its open positino
+            if (last) {
+                if (oy > height * 0.5 || (vy > 0.5 && dy > 0)) {
+                    closeDrawer(vy);
+                } else {
+                    openDrawer();
+                }
+            }
+            // when the user keeps dragging, we just move the sheet according to
+            // the cursor position
+            else {
+                api.start({ y: oy, immediate: true });
+            }
+        },
+        { from: () => [0, y.get()], filterTaps: true, bounds: { top: 0 }, rubberband: true }
+    );
+
+    useEffect(() => {
+        if (isOpen) {
+            openDrawer();
+        }
+    }, [api, isOpen, openDrawer]);
+
+    if (!isOpen) {
         return null;
     }
 
+    const display = y.to(py => (py < height ? 'block' : 'none'));
+
     return (
         <Portal>
-            <div className={classNames(cls.Drawer, mods, [className, theme, 'app_drawer'])}>
-                <Overlay onClick={close} />
-                <div className={cls.content}>{children}</div>
+            <div className={classNames(cls.Drawer, {}, [className, theme, 'app_drawer'])}>
+                <Overlay onClick={closeDrawer} />
+                <Spring.a.div
+                    className={cls.sheet}
+                    style={{ display, bottom: `calc(-100vh + ${height - 100}px)`, y }}
+                    {...bind()}
+                >
+                    {children}
+                </Spring.a.div>
             </div>
         </Portal>
     );
 });
+
+const DrawerComponent = memo((props: TDrawerProps) => {
+    const { isLoaded } = useAnimationLibs();
+
+    if (!isLoaded) {
+        return null;
+    }
+
+    return <DrawerContent {...props} />;
+});
+
+export const Drawer = memo((props: TDrawerProps) => (
+    <AnimationProvider>
+        <DrawerComponent {...props} />
+    </AnimationProvider>
+));
